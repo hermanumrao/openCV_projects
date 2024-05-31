f5c36e2a8f8a5c19d373b0869dce1ea8bf36f91c
hermanumrao <hermanumrao@gmail.com>
2024-05-31 22:13:02 +0530

    completed project2 of document scanner

--- proj2_docScan/docScan.cpp
+++ proj2_docScan/docScan.cpp
@@ -32,44 +32,83 @@ vector<Point> getcontours(Mat img) {
   vector<vector<Point>> conPoly(contours.size());
   vector<Rect> boundRect(contours.size());
 
+  vector<Point> biggest;
+  int maxArea = 0;
+
   for (int i = 0; i < contours.size(); i++) {
     int area = contourArea(contours[i]);
-    cout << area << endl;
+    // cout << area << endl;
     string objectType;
     if (area > 1000) {
       float peri = arcLength(contours[i], true);
       approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
-      cout << conPoly[i].size() << endl;
-      boundRect[i] = boundingRect(conPoly[i]);
+      // cout << conPoly[i].size() << endl;
 
-      int objCor = (int)conPoly[i].size();
-
-      if (objCor == 3)
-        objectType = "Triangle";
-      else if (objCor == 4) {
-        float aspRatio = (float)boundRect[i].width / (float)boundRect[i].height;
-        cout << aspRatio << endl;
-        if (aspRatio > 0.95 && aspRatio < 1.05)
-          objectType = "square";
-        else
-          objectType = "rectangle";
-      } else if (objCor > 4)
-        objectType = "circle";
-
-      drawContours(img, conPoly, i, Scalar(0, 0, 0), 5);
-      rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(255, 0, 255),
-                5);
-      putText(img, objectType, {boundRect[i].x, boundRect[i].y - 5},
-              FONT_HERSHEY_PLAIN, 1, Scalar(0, 255, 69), 2);
+      if (area > maxArea && conPoly[i].size() == 4) {
+        biggest = {conPoly[i][0], conPoly[i][1], conPoly[i][2], conPoly[i][3]};
+        maxArea = area;
+      }
     }
+  }
+  return biggest;
+}
+
+void drawPoints(vector<Point> points, Scalar color) {
+  for (int i = 0; i < 4; i++) {
+    circle(imgOriginal, points[i], 10, color, FILLED);
+    putText(imgOriginal, to_string(i), points[i], FONT_HERSHEY_PLAIN, 4, color,
+            4);
   }
 }
 
+vector<Point> reorder(vector<Point> points) {
+  vector<Point> newPoints;
+  vector<int> sumPoints, subPoints;
+
+  for (int i = 0; i < 4; i++) {
+    sumPoints.push_back(points[i].x + points[i].y);
+    subPoints.push_back(points[i].x - points[i].y);
+  }
+
+  newPoints.push_back(points[min_element(sumPoints.begin(), sumPoints.end()) -
+                             sumPoints.begin()]); // 0
+  newPoints.push_back(points[max_element(subPoints.begin(), subPoints.end()) -
+                             subPoints.begin()]); // 1
+  newPoints.push_back(points[min_element(subPoints.begin(), subPoints.end()) -
+                             subPoints.begin()]); // 2
+  newPoints.push_back(points[max_element(sumPoints.begin(), sumPoints.end()) -
+                             sumPoints.begin()]); // 3
+
+  return newPoints;
+}
+
+Mat getWarp(Mat img, vector<Point> points, float w, float h) {
+  Point2f src[4] = {points[0], points[1], points[2], points[3]};
+  Point2f dst[4] = {{0.0f, 0.0f}, {w, 0.0f}, {0.0f, h}, {w, h}};
+
+  Mat matrix = getPerspectiveTransform(src, dst);
+  warpPerspective(img, imgWarp, matrix, Point(w, h));
+  return imgWarp;
+}
+
 int main(int argc, char *argv[]) {
-  string path = "/home/herman/workspace/openCV_projects/resources/pap2.jpeg";
+  string path = "/home/herman/workspace/openCV_projects/resources/pap1.jpeg";
   imgOriginal = imread(path);
 
-  imgThre = preprocess(imgOriginal);
+  imgThre = preprocess(imgOriginal); // preprocessing         Step1
+
+  initialPoints = getcontours(imgThre); // Get contours          Step2
+  docPoints = reorder(initialPoints);   // reordering points to get it in order
+
+  imgWarp = getWarp(imgOriginal, docPoints, w, h); // wrapping the image Step3
+
+  int cropVal = 5; // cropping              Step4
+  Rect roi(cropVal, cropVal, w - (2 * cropVal), h - (2 * cropVal));
+  imgCrop = imgWarp(roi);
+
+  imshow("document", imgOriginal);
+  imshow("scanned document", imgCrop);
+  waitKey(0);
 
   return 0;
 }
